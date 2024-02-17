a1cadf4aca7260c9f981838327229ae5e3e5e945
Peter Roest Rønne <77733864+PeterRonne@users.noreply.github.com>
2024-02-17 20:09:34 +0100

    initial commit

--- src/gui/TicTacToe.java
+++ src/gui/TicTacToe.java
@@ -1,11 +1,36 @@
 package gui;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class TicTacToe extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Tic Tac Toe");
+        primaryStage.setResizable(false);
 
+        BorderPane pane = new BorderPane();
+        this.initContent(pane);
+
+        Scene scene = new Scene(pane);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    private void initContent(BorderPane pane) {
+
+
+
+    }
+
+    private void createGuiBoard() {
+        GridPane gridPane = new GridPane();
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+            }
+        }
     }
 }
