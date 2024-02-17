56f8635c5bbda838f9981d4f10184722a0771830
Peter Roest Rønne <77733864+PeterRonne@users.noreply.github.com>
2024-02-17 20:32:21 +0100

    gameBoard added to gui

--- src/gui/TicTacToe.java
+++ src/gui/TicTacToe.java
@@ -1,6 +1,8 @@
 package gui;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
@@ -22,15 +24,32 @@ public class TicTacToe extends Application {
 
     private void initContent(BorderPane pane) {
 
+        pane.setCenter(GameBoard());
 
 
     }
 
-    private void createGuiBoard() {
-        GridPane gridPane = new GridPane();
+    private GridPane GameBoard() {
+        GridPane gameBoard = new GridPane();
         for (int row = 0; row < 3; row++) {
             for (int col = 0; col < 3; col++) {
+                Tile tile = new Tile(row, col, this::handleTileClick);
+                gameBoard.add(tile, row, col);
             }
         }
+        return gameBoard;
     }
+
+    private void handleTileClick(ActionEvent actionEvent) {
+        // Retrieve the source of the event, which should be the Tile button
+        Tile clickedTile = (Tile) actionEvent.getSource();
+
+        // Retrieve the row and column information from the clicked Tile
+        int clickedRow = clickedTile.getRow();
+        int clickedCol = clickedTile.getCol();
+
+        System.out.println("Button clicked at row: " + clickedRow + ", column: " + clickedCol);
+    }
+
+
 }
---
+++ src/gui/Tile.java
@@ -0,0 +1,61 @@
+package gui;
+
+import application.model.Marker;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+
+public class Tile extends Button {
+    public static final int TILE_SIZE = 100;
+    private int row;
+    private int col;
+    private Marker marker;
+
+    public Tile(int row, int col, EventHandler<ActionEvent> action) {
+        this.row = row;
+        this.col = col;
+        this.marker = null;
+        this.setPrefSize(TILE_SIZE, TILE_SIZE);
+        this.setOnAction(action);
+    }
+
+    public void setMarker(Marker marker) {
+        this.marker = marker;
+    }
+
+    public void update(Marker marker) {
+        this.marker = marker;
+        this.setText("" + marker);
+        this.setOnAction(null);
+    }
+
+//    public void setBaseStyle() {
+//        this.setStyle("-fx-font-size:45; -fx-background-color: #ffffff; -fx-border-color: #000000; -fx-border-width: 2px; -fx-border-radius: 0px;");
+//    }
+//
+//    public void setWinningStyle() {
+//        this.setStyle("-fx-font-size:45; -fx-background-color: #7AE582; -fx-border-color: #000000; -fx-border-width: 2px; -fx-border-radius: 0px;");
+//    }
+//
+//    public void setHelperStyle() {
+//        this.setStyle("-fx-font-size:45; -fx-background-color: #00A5CF; -fx-border-color: #000000; -fx-border-width: 2px; -fx-border-radius: 0px;");
+//    }
+
+    public Marker getMarker() {
+        return marker;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    @Override
+    public String toString() {
+        return "{" + row + ", " + col + "}";
+    }
+}
